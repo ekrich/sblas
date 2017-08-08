5eda97d2e39b6aebabde17ed32769ec629a14e90
ekrich <ekrichardson@gmail.com>
2017-08-08 14:43:34 -0400

    Use setup from Scala Native for formatting

--- unit-tests/src/test/scala/NativeTest1Suite.scala
+++ unit-tests/src/test/scala/NativeTest1Suite.scala
@@ -4,18 +4,17 @@ import utest._
 import scala.scalanative.native._
 
 object NativeTest1Suite extends TestSuite {
-
   def tests = this {
     //Zone { implicit z =>
-      // do stuff in Zone here
-      'test {
-        // ok for Zone here
-        Zone { implicit z =>
-          val f = alloc[CFloat]
-          !f = 1.0f
-          assert(!f == 1.0)
-        }
+    // do stuff in Zone here
+    'test {
+      // ok for Zone here
+      Zone { implicit z =>
+        val f = alloc[CFloat]
+        !f = 1.0f
+        assert(!f == 1.0)
       }
+    }
     //}
   }
 }
