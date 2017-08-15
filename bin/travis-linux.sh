ed60afebbdfbaf21de6413528f2a19412a97e140
ekrich <ekrichardson@gmail.com>
2017-08-15 12:55:51 -0400

    Upgrade to clang 4.0

--- bin/travis-linux.sh
+++ bin/travis-linux.sh
@@ -10,7 +10,7 @@ IFS=$'\n\t'
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
 sudo apt-get install -y -qq \
-    clang++-3.8 \
+    clang++-4.0 \
     libgc-dev \
     libunwind8-dev \
     libatlas-base-dev
@@ -18,7 +18,7 @@ sudo apt-get install -y -qq \
 # Install re2
 # Starting from Ubuntu 16.04 LTS, it'll be available as http://packages.ubuntu.com/xenial/libre2-dev
 sudo apt-get install -y make
-export CXX=clang++-3.8
+export CXX=clang++-4.0
 git clone https://code.googlesource.com/re2
 pushd re2
 git checkout 2017-03-01
