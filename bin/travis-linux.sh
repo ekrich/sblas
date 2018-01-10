4725164f5817d007bec45d75bc68d9e0f0318dea
ekrich <ekrichardson@gmail.com>
2018-01-10 08:14:26 -0500

    Some more Travis changes

--- bin/travis-linux.sh
+++ bin/travis-linux.sh
@@ -10,16 +10,15 @@ IFS=$'\n\t'
 sudo find /usr -name "*libunwind*" -delete
 
 # Use pre-bundled clang
-    export PATH=/usr/local/clang-5.0.0/bin:$PATH
-    export CXX=clang++
+export PATH=/usr/local/clang-5.0.0/bin:$PATH
+export CXX=clang++
 
-    # Install Boehm GC and libunwind
+# Install Boehm GC and libunwind
 sudo apt-get install libgc-dev libunwind8-dev libatlas-base-dev
 
 # Install re2
 # Starting from Ubuntu 16.04 LTS, it'll be available as http://packages.ubuntu.com/xenial/libre2-dev
 sudo apt-get install -y make
-export CXX=clang++-3.9
 git clone https://code.googlesource.com/re2
 pushd re2
 git checkout 2017-03-01
