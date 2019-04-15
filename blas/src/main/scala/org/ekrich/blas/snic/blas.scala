eb94b01297884d225e22643470ce35278f061c13
ekrich <ekrichardson@gmail.com>
2019-04-15 10:28:29 -0700

    Fix formatting

--- blas/src/main/scala/org/ekrich/blas/snic/blas.scala
+++ blas/src/main/scala/org/ekrich/blas/snic/blas.scala
@@ -1,18 +1,17 @@
 /** Copyright 2017-2019 Eric K Richardson
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
-
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ekrich.blas.snic
 
 import scala.scalanative.native._
@@ -22,7 +21,7 @@ import scala.scalanative.native.complex.{CDoubleComplex, CFloatComplex}
  * [[http://www.netlib.org/blas/#_cblas]] (for header file)
  * Documentation Reference:
  * [[https://developer.apple.com/documentation/accelerate/blas?language=objc]]
-  * REFERENCE BLAS Version 3.8.0
+ * REFERENCE BLAS Version 3.8.0
  */
 @link("cblas")
 @extern
@@ -498,8 +497,8 @@ object blas {
 }
 
 /**
-  * Used for level 2 and 3 BLAS
-  */
+ * Used for level 2 and 3 BLAS
+ */
 object blasEnums {
   //enums
   type CBLAS_ORDER = CInt
