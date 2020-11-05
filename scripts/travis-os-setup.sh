4c47a6aacb379aeb8654c7b89fa02a92eedc0ea9
ekrich <ekrichardson@gmail.com>
2020-11-04 16:29:52 -0800

    Fix script

--- scripts/travis-os-setup.sh
+++ scripts/travis-os-setup.sh
@@ -6,9 +6,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    # Nothing needed
-else
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    # Nothing for osx
+
     # Per https://github.com/scala-native/scala-native/pull/1240/
     sudo apt-get update
 
