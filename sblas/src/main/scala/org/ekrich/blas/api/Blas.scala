6b202dac19d26099d8d3844ed0d1172246ea8981
ekrich <ekrichardson@gmail.com>
2023-11-16 09:47:55 -0800

    Checkpoint commit

--- sblas/src/main/scala/org/ekrich/blas/api/Blas.scala
+++ sblas/src/main/scala/org/ekrich/blas/api/Blas.scala
@@ -14,11 +14,60 @@ package api
 object Blas {
   import scala.scalanative.unsafe._
 
-  def dnrm2(vector: Array[Double]): Double = dnrm2(vector, stride = 1)
+  /** L2 norm (Euclidian length) of a vector (double-precision).
+   *
+   *  @param X
+   *    Vector X.
+   *  @return
+   *    See description above.
+   */
+  def dnrm2(X: Array[Double]): Double = dnrm2(X, incX = 1)
 
-  def dnrm2(vector: Array[Double], stride: Int): Double =
-    dnrm2(vector, stride, vector.length)
+  /** L2 norm (Euclidian length) of a vector (double-precision).
+   *
+   *  @param X
+   *    Vector X.
+   *  @param incX
+   *    Stride within X.
+   *  @return
+   *    See description above.
+   */
+  def dnrm2(X: Array[Double], incX: Int): Double =
+    dnrm2(X, incX, X.length)
 
-  def dnrm2(vector: Array[Double], stride: Int, n: Int): Double =
-    unsafe.blas.cblas_dnrm2(n, vector.at(0), stride)
+  /** @param X
+   *  @param incX
+   *  @param N
+   *  @return
+   */
+  def dnrm2(X: Array[Double], incX: Int, N: Int): Double =
+    unsafe.blas.cblas_dnrm2(N, X.at(0), incX)
+
+  /** Dot product of two single-precision vectors plus an initial
+   *  single-precision value.
+   *
+   *  @param N
+   *    The number of elements in the vectors.
+   *  @param alpha
+   *    The initial value to add to the dot product.
+   *  @param X
+   *    Vector X.
+   *  @param incX
+   *    Stride within X. For example, if incX is 7, every 7th element is used.
+   *  @param Y
+   *    Vector Y.
+   *  @param incY
+   *    Stride within Y. For example, if incY is 7, every 7th element is used.
+   *  @return
+   *    See description above.
+   */
+  def sdsdot(
+      N: Int,
+      alpha: Float,
+      X: Array[Float],
+      incX: CInt,
+      Y: Array[Float],
+      incY: Int
+  ): Float =
+    unsafe.blas.cblas_sdsdot(N, alpha, X.at(0), incX, Y.at(0), incY = incY)
 }
